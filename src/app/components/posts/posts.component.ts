e504d5f0048e488b46992f07ec7071e080e66efa
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-01-29 23:59:29 +0100

    add new file

---
+++ src/app/components/posts/posts.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../post.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrl: './posts.component.scss',
+})
+export class PostsComponent implements OnInit {
+  constructor(private postService: PostService) {}
+  postList: any[] = [];
+  post = {};
+
+  ngOnInit(): void {
+    this.postService.getPosts().subscribe((val) => {
+      console.log(val);
+    });
+  }
+
+  getPostById(id: number) {
+    this.postService.getPostById(id).subscribe((val) => {
+      console.log(val);
+      this.post = val;
+    });
+  }
+}
