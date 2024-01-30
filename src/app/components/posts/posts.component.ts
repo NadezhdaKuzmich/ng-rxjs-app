6a4db9eafca84692dd0c3b424d069edb47fd077c
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-01-31 00:14:48 +0100

    add new section

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -12,20 +12,53 @@ export class PostsComponent implements OnInit {
     private postService: PostService,
     private userService: UserService
   ) {}
-  postList: any[] = [];
   post: any = null;
+  user: any = [];
 
   ngOnInit() {
-    this.postService.getPosts().subscribe((val) => {
-      console.log(val);
+    this.postService.getPosts().subscribe((res) => {
+      console.log('POSTS:', res);
     });
-    this.userService.getAllUsers().subscribe((val) => console.log(val));
+
+    this.userService
+      .getAllUsers()
+      .subscribe((res) => console.log('USERS:', res));
+
+    this.userService
+      .addUser({
+        id: 6,
+        userName: 'Igor',
+        email: 'user6@email.com',
+      })
+      .subscribe((res) => console.log('POST METHOD:', res));
+
+    this.userService
+      .getAllUsers()
+      .subscribe((res) => console.log('USERS:', res));
+
+    this.userService.getUserById(6).subscribe((user) => {
+      console.log(user);
+    });
+
+    this.userService
+      .deleteUser(6)
+      .subscribe((res) => console.log('DELETE METHOD:', res));
+
+    this.userService
+      .getAllUsers()
+      .subscribe((res) => console.log('USERS:', res));
   }
 
   getPostById(id: number) {
-    this.postService.getPostById(id).subscribe((val) => {
-      console.log(val);
-      this.post = val;
+    this.postService.getPostById(id).subscribe((post) => {
+      console.log(post);
+      this.post = post;
     });
+  }
+
+  getUserByName(userName: string) {
+    this.userService
+      .getUserByName(userName)
+      .subscribe((user) => console.log('GET USER WITH HttpParams', user));
   }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,14 +1,57 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   constructor(private http: HttpClient) {}
-  URL = 'api/';
+  URL = 'api/users';
 
   getAllUsers() {
-    return this.http.get(this.URL + 'users');
+    return this.http.get(this.URL);
+  }
+
+  getUserById(id: number) {
+    return this.http.get(`${this.URL}?id=${id}`);
+  }
+
+  addUser(payload: any) {
+    return this.http.post(this.URL, payload);
+  }
+
+  deleteUser(id: number) {
+    return this.http.delete(`${this.URL}/${id}`);
+  }
+
+  // v1
+  // getUserByName(name: string): Observable<any> {
+  //   return this.http.get(this.URL, {
+  //     params: new HttpParams().set('userName', name),
+  //   });
+  // }
+
+  // v2
+  // getUserByName(name: string): Observable<any> {
+  //   const httpParams = new HttpParams({
+  //     fromObject: {
+  //       userName: name,
+  //     },
+  //   });
+  //   return this.http.get(this.URL, {
+  //     params: httpParams,
+  //   });
+  // }
+
+  // v3
+  getUserByName(name: string): Observable<any> {
+    const httpParams = new HttpParams({
+      fromString: `userName=${name}`,
+    });
+    //localhost:4200?userName=Max
+    return this.http.get(this.URL, {
+      params: httpParams,
+    });
   }
 }
